f1d7ced4aaed7e19b7b0aa48ba87e96b2989ffd8
yuchi <yuchi8943@gmail.com>
2022-12-01 15:31:31 +0800

    add Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,12 +13,14 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
+
+
     
 
     public function handle($request, Closure $next)
-{
-    return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-}
+    {
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
 }
